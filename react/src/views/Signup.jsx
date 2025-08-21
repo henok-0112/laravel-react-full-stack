d351aea57619b9b8ca6f6266f1d4d7fd0ae062ce
henok-0112 <henaman4885@gmail.com>
2025-08-21 18:07:30 +0300

    third commit

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+
 export default function Signup() {
-    return <div>Signup</div>;
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form action="" onSubmit={onSubmit}>
+                    <h1>Signup for free</h1>
+                    <input type="text" placeholder="Full Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <input
+                        type="password"
+                        placeholder="Password Confirmation"
+                    />
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">
+                        Already Registered? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
 }
